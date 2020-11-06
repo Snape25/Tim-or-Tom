b0372794559fcad6dfd195ffd9d3b72b4491bd60
Nandagopal <65445810+Snape25@users.noreply.github.com>
2020-11-06 18:13:12 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,49 @@
+
+import random
+from inputimeout import inputimeout, TimeoutOccurred
+
+
+def game():
+  ab = 1
+  score = 0
+  while ab == 1:
+    
+     try:
+        choices = ["tim","tom"]
+        choice = random.choice(choices)
+        print(choice)
+        user = inputimeout(prompt='>>', timeout=2)
+        if user == "tim" and choice =="tom":
+             print("YOU SCORED A POINT")
+             score += 1
+        elif user == "tom" and choice =="tim":
+            print("YOU SCORED A POINT")
+            score += 1
+        else:
+             print("YOU LOST")
+             print("Your score is: {}".format(score) )
+             ab = ab-1
+
+
+     except TimeoutOccurred:
+       print("Time up")
+       break
+
+    
+
+
+
+print("Welcome to Tim or tom.Type start to begin")
+pick = input("Input:   ")
+pick = str(pick)
+if pick =="start":
+    game()
+
+
+
+
+
+
+
+
+
